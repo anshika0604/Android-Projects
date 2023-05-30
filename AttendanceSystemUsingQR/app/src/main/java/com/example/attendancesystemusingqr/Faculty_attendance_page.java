8c21d95ebbb685a7d3cec9decf4966dcd44edb5e
Anshika <anshikanegi.cs19@gmail.com>
2023-05-30 23:53:54 +0530

    updated

--- AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_attendance_page.java
+++ AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_attendance_page.java
@@ -3,11 +3,18 @@ package com.example.attendancesystemusingqr;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
-public class Faculty_attendance_page extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Faculty_attendance_page extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,39 @@ public class Faculty_attendance_page extends AppCompatActivity {
     public void generate_qr(View view) {
         Intent intent = new Intent(Faculty_attendance_page.this, QR_generator.class);
         startActivity(intent);
+    }
+    public void set_date(View view) {
+        showDateDialog();
+    }
+    public void showDateDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+    public void set_time(View view) {
+        showTimeDialog();
+    }
+    public void showTimeDialog() {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(Faculty_attendance_page.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay,int minute) {
+            }
+        }, hour, minute, false);
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
     }
 }
