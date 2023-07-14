e2059345af4b053facfde657dc35a57c745d7b99
Anshika <anshikanegi.cs19@gmail.com>
2023-07-14 22:30:28 +0530

    updated

--- AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_selectionpage.java
+++ AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_selectionpage.java
@@ -1,18 +1,26 @@
 package com.example.attendancesystemusingqr;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Faculty_selectionpage extends AppCompatActivity {
 
     Button view, mark, btnSignOut;
+    FloatingActionButton fab;
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +29,47 @@ public class Faculty_selectionpage extends AppCompatActivity {
         // Toolbar Styling and Back Button
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_foreground);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_foreground);
 
+        // Navigation Views
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fab = findViewById(R.id.fab);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,myToolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null) {
+
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                return true;
+            } else if (itemId == R.id.add) {
+                startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                finish();
+                return true;
+            } else if (itemId == R.id.mark) {
+                startActivity(new Intent(getApplicationContext(), Faculty_attendance_page.class));
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                finish();
+                return true;
+            } else if (itemId == R.id.view) {
+                startActivity(new Intent(getApplicationContext(), View_details.class));
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                finish();
+                return true;
+            }
+            return false;
+        });
         // Log Out
         btnSignOut = findViewById(R.id.signOut);
 
