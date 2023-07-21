6509987ccce08bfd9df2cacf6cab490d68b8bff9
Anshika <anshikanegi.cs19@gmail.com>
2023-07-21 13:34:30 +0530

    updated

--- AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_selectionpage.java
+++ AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_selectionpage.java
@@ -1,12 +1,16 @@
 package com.example.attendancesystemusingqr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,9 +33,13 @@ public class Faculty_selectionpage extends AppCompatActivity {
         // Toolbar Styling and Back Button
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_foreground);
+
+        // Get Email
+        Intent intent = getIntent();
+        String email =  intent.getStringExtra("email");
 
         // Navigation Views
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -42,15 +50,63 @@ public class Faculty_selectionpage extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,myToolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
-        if(savedInstanceState == null) {
-
+        if(savedInstanceState == null ) {
             navigationView.setCheckedItem(R.id.nav_home);
         }
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_home) {
+                    Log.d("Hello", "i am selected "+item.getItemId());
+                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                    finish();
+
+                } else if (item.getItemId() == R.id.nav_add) {
+                    Log.d("Hello", "i am selected "+item.getItemId());
+                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                    finish();
+
+                } else if (item.getItemId() == R.id.nav_mark) {
+                    Log.d("Hello", "i am selected "+item.getItemId());
+                    Intent intent1 = new Intent(getApplicationContext(), Faculty_attendance_page.class);
+                    intent1.putExtra("email",email);
+                    startActivity(intent1);
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                    finish();
+
+                } else if (item.getItemId() == R.id.nav_view_attendance) {
+                    Log.d("Hello", "i am selected "+item.getItemId());
+                    startActivity(new Intent(getApplicationContext(), View_details.class));
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                    finish();
+
+                } else if (item.getItemId() == R.id.nav_profile) {
+                    Log.d("Hello", "i am selected "+item.getItemId());
+                    startActivity(new Intent(getApplicationContext(), Profile.class));
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                    finish();
+
+                }  else if (item.getItemId() == R.id.nav_logout) {
+                    Log.d("Hello", "i am selected "+item.getItemId());
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(getApplicationContext(), Faculty_loginpage.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                return true;
+            }
+        });
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
+                startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                finish();
                 return true;
             } else if (itemId == R.id.add) {
                 startActivity(new Intent(getApplicationContext(), Dashboard.class));
@@ -58,7 +114,9 @@ public class Faculty_selectionpage extends AppCompatActivity {
                 finish();
                 return true;
             } else if (itemId == R.id.mark) {
-                startActivity(new Intent(getApplicationContext(), Faculty_attendance_page.class));
+                Intent intent1 = new Intent(getApplicationContext(), Faculty_attendance_page.class);
+                intent1.putExtra("email",email);
+                startActivity(intent1);
                 overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
                 finish();
                 return true;
@@ -68,28 +126,22 @@ public class Faculty_selectionpage extends AppCompatActivity {
                 finish();
                 return true;
             }
+
             return false;
         });
-        // Log Out
-        btnSignOut = findViewById(R.id.signOut);
-
-
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Faculty_loginpage.class);
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
                 startActivity(intent);
                 finish();
             }
         });
+
         // Button Functions
 
         mark = findViewById(R.id.mark);
         view = findViewById(R.id.view);
-
-        Intent intent = getIntent();
-        String email =  intent.getStringExtra("email");
 
         mark.setOnClickListener(new View.OnClickListener() {
             @Override
