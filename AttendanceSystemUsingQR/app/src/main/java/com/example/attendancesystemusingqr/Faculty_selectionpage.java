286524d3781ca722438731a210d305aacb219546
Anshika <anshikanegi.cs19@gmail.com>
2023-07-25 13:59:43 +0530

    updated

--- AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_selectionpage.java
+++ AttendanceSystemUsingQR/app/src/main/java/com/example/attendancesystemusingqr/Faculty_selectionpage.java
@@ -7,12 +7,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,6 +25,9 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Faculty_selectionpage extends AppCompatActivity {
 
     Button view, mark, btnSignOut, profileView;
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, mark1, view1, profile, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +38,61 @@ public class Faculty_selectionpage extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_foreground);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_foreground);
 
         // Get Email
         Intent intent = getIntent();
         String email =  intent.getStringExtra("email");
+
+
+        // Navigation Drawer
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.nav_home);
+        mark1 = findViewById(R.id.nav_mark);
+        view1 = findViewById(R.id.nav_view);
+        profile = findViewById(R.id.nav_profile);
+        logout = findViewById(R.id.nav_logout);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer(drawerLayout);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
+        mark1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Faculty_selectionpage.this, Faculty_attendance_page.class, email);
+            }
+        });
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Faculty_selectionpage.this, View_details.class, email);
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Faculty_selectionpage.this, Profile_Faculty.class, email);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                redirectActivity(Faculty_selectionpage.this, Faculty_loginpage.class, email);
+            }
+        });
 
 
         // Button Functions
@@ -45,7 +100,6 @@ public class Faculty_selectionpage extends AppCompatActivity {
         mark = findViewById(R.id.mark);
         view = findViewById(R.id.view);
         profileView = findViewById(R.id.profileview);
-        btnSignOut = findViewById(R.id.signOut);
 
         mark.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,16 +129,26 @@ public class Faculty_selectionpage extends AppCompatActivity {
                 finish();
             }
         });
-
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Faculty_loginpage.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    }
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity, String email) {
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("email", email);
+        activity.startActivity(intent);
+        activity.finish();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
 }
